e885d1d1ebc3eab99f76beb8d935e05b19bec87a
Damian Berning <d.berning@mobizcorp.de>
2015-10-01 16:16:26 +0200

    Tutorial bis Adding Navigation

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
-export class App {
-  configureRouter(config, router){
-    config.title = 'Aurelia';
-    config.map([
-      { route: ['','welcome'],  name: 'welcome',      moduleId: 'welcome',      nav: true, title:'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title:'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title:'Child Router' }
-    ]);
+export class Welcome {
+	heading = 'Welcome to the Aurelia Navigation App!';
+	firstName = 'John';
+	lastName = 'Doe';
 
-    this.router = router;
-  }
+	get fullName() {
+		return `${this.firstName} ${this.lastName}`;
+	}
+
+	submit() {
+		alert(`Welcome, ${this.fullName}!`);
+	}
 }
